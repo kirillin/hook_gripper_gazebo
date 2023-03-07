198ac32df31a9e1f71d4b68cd4f3754ebb0b78f7
kirillin <kaartemov@gmail.com>
2023-03-07 20:34:30 +0300

    Add ros servise server to grasp

--- include/hook_gripper_gazebo/gazebo_ros_hook_gripper.hpp
+++ include/hook_gripper_gazebo/gazebo_ros_hook_gripper.hpp
@@ -9,14 +9,17 @@
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
 #include <gazebo/common/common.hh>
-// #include <gazebo/sensors/sensors.hh>
 #include <gazebo/transport/transport.hh>
 
 #include <thread>
 #include "ros/ros.h"
 #include "ros/callback_queue.h"
 #include "ros/subscribe_options.h"
+#include <ros/advertise_options.h>
+#include <ros/advertise_service_options.h>
 #include "std_msgs/Bool.h"
+#include <std_srvs/SetBool.h>
+#include <std_srvs/Empty.h>
 
 
 const std::string PLUGIN_NAME = "HOOK_GRIPPER_PLUGIN";
@@ -28,27 +31,22 @@ namespace gazebo
 
     public: 
       HookGripperPlugin();
-      // TODO add destructor
+      virtual ~HookGripperPlugin();
       virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
       void OnContactsMsg(ConstContactsPtr &_msg);
       void Attach();
       void Detach();
-
       void OnUpdate();
       void OnReset();   
 
     private:
-      // parameters from sdf
       std::string gripper_robot_name;
       std::string gripper_link_name;
       std::string objects_robot_name;
       std::string objects_link_name;
-
       std::string gripper_collision;
       std::string objects_collision;
-
       std::string grasping_model_name;
-
       std::string gripperName;
 
       physics::ModelPtr model;
@@ -59,16 +57,21 @@ namespace gazebo
       gazebo::transport::SubscriberPtr contacts_sub;
 
       event::ConnectionPtr update_connection;
-      event::ConnectionPtr reset_connection;
 
       mutable boost::mutex mutex;
       double min_collision_depth;
       bool ready_to_attach;
       bool attached;
 
+      bool check_collision(const std::string& c1, const std::string& c2);
+      void get_grasp_model_name(std::string& model_name, const std::string& c1, const std::string& c2);
+
       //  ROS stuff
       std::unique_ptr<ros::NodeHandle> rosNode;
       
+      ros::Publisher rosPubReady;
+      int ready_publisher_connect_count_;
+
       ros::Publisher rosPubState;
       int state_publisher_connect_count_;
 
@@ -79,7 +82,9 @@ namespace gazebo
       void QueueThread();
       void Connect();
       void Disconnect();
-      // bool OnServiceCallback(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res);
+      void Connect2();
+      void Disconnect2();
+      bool GraspServiceCallback(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res);
 
   };
 }
--- src/gazebo_ros_hook_gripper.cpp
+++ src/gazebo_ros_hook_gripper.cpp
@@ -5,6 +5,15 @@ namespace gazebo
     GZ_REGISTER_MODEL_PLUGIN(HookGripperPlugin)
 
     HookGripperPlugin::HookGripperPlugin() {}
+    HookGripperPlugin::~HookGripperPlugin() 
+    {
+        this->update_connection_.reset();
+
+        rosQueue.clear();
+        rosQueue.disable();
+        rosNode->shutdown();
+        rosQueueThread.join();
+    }
 
     void HookGripperPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
     {
@@ -17,8 +26,8 @@ namespace gazebo
         this->attached = false;
         this->ready_to_attach = false;
         this->grasping_model_name = "";
-        physics::PhysicsEnginePtr physics = this->model->GetWorld()->Physics();
-        this->fixedJoint = physics->CreateJoint("revolute");
+        // physics::PhysicsEnginePtr physics = this->model->GetWorld()->Physics();
+        // this->fixedJoint = physics->CreateJoint("revolute");
 
 
         // GET ATRIBUTES
@@ -32,7 +41,7 @@ namespace gazebo
             gzmsg << "Must be set all parameters! Plugin will not work!\n";
             // TODO throw exception
         } else {
-            // TODO add checking for values
+            // TODO add checking for validity of values
             this->gripper_robot_name = gripper_robot_name->Get<std::string>();
             this->gripper_link_name = gripper_link_name->Get<std::string>();
             this->objects_robot_name = objects_robot_name->Get<std::string>(); // must be complex name to be unique for grasping objects in a world
@@ -55,8 +64,6 @@ namespace gazebo
         std::string topicContactsName = "~/physics/contacts"; // /gazebo/default/physics/contacts -> gazebo.msgs.Contacts
         this->contacts_sub = this->transport_node->Subscribe(topicContactsName, &HookGripperPlugin::OnContactsMsg, this);
 
-        // TODO publish ready_to_attach -> grasping_state
-        // TODO service for attach
         // ROS initialization
         if (!ros::isInitialized())
         {
@@ -69,103 +76,124 @@ namespace gazebo
 
         this->rosNode.reset(new ros::NodeHandle("gazebo_hook_gripper_client"));
 
-        // state publisher
+        // ready to attach publisher
         ros::AdvertiseOptions ao = ros::AdvertiseOptions::create<std_msgs::Bool>(
-                                            "/" + this->gripper_link_name + "/state", 1,
-                                            std::bind(&HookGripperPlugin::Connect, this),
-                                            std::bind(&HookGripperPlugin::Disconnect, this),
+                                            "/" + this->gripper_link_name + "/ready", 1,
+                                            boost::bind(&HookGripperPlugin::Connect, this),
+                                            boost::bind(&HookGripperPlugin::Disconnect, this),
                                             ros::VoidPtr(), &this->rosQueue);
-        this->rosPubState = this->rosNode->advertise(ao);
+        this->rosPubReady = this->rosNode->advertise(ao);
+        this->ready_publisher_connect_count_ = 0;
+
+        // state publisher
+        ros::AdvertiseOptions ao2 = ros::AdvertiseOptions::create<std_msgs::Bool>(
+                                            "/" + this->gripper_link_name + "/state", 1,
+                                            boost::bind(&HookGripperPlugin::Connect2, this),
+                                            boost::bind(&HookGripperPlugin::Disconnect2, this),
+                                            ros::VoidPtr(), &this->rosQueue);
+        this->rosPubState = this->rosNode->advertise(ao2);
+        this->state_publisher_connect_count_ = 0;
 
         // service to grasp (attach/detach object)
-        // ros::AdvertiseServiceOptions aso = ros::AdvertiseServiceOptions::create<std_srvs::Empty>(
-        //                                     "on", std::bind(&HookGripperPlugin::OnServiceCallback,
-        //                                     this, _1, _2), ros::VoidPtr(), &queue_);
-        // this->rosSrvGrasp = this->rosNode->advertiseService(aso1);
+        ros::AdvertiseServiceOptions aso = ros::AdvertiseServiceOptions::create<std_srvs::SetBool>(
+                                            "grasp", boost::bind(&HookGripperPlugin::GraspServiceCallback, this, _1, _2), 
+                                            ros::VoidPtr(), &this->rosQueue);
+        this->rosSrvGrasp = this->rosNode->advertiseService(aso);
 
         this->rosQueueThread = std::thread(std::bind(&HookGripperPlugin::QueueThread, this));
 
-        update_connection = event::Events::ConnectWorldUpdateBegin(boost::bind(&HookGripperPlugin::OnUpdate, this));
-        reset_connection = event::Events::ConnectWorldReset(boost::bind(&HookGripperPlugin::OnReset, this));
+        update_connection = event::Events::ConnectWorldUpdateBegin(std::bind(&HookGripperPlugin::OnUpdate, this));
+        reset_connection = event::Events::ConnectWorldReset(std::bind(&HookGripperPlugin::OnReset, this));
 
         gzmsg << "Loaded " << PLUGIN_NAME << std::endl;
+    }
+
+    // check if gripper and ok-to-grasp objects in collision
+    bool HookGripperPlugin::check_collision(const std::string& c1, const std::string& c2) 
+    {
+        bool is_collision = false;
+        if (c1.find(this->gripper_collision) !=  std::string::npos) {
+            if (c2.find(this->objects_link_name) !=  std::string::npos) {
+                is_collision = true;
+            }
+        } else if (c2.find(this->gripper_collision) !=  std::string::npos) {
+            if (c1.find(this->objects_link_name) !=  std::string::npos) {
+                is_collision = true;
+            }
+        }
+        return is_collision;
+    }
+
+    // extract the grasp model name from string of names of collisions
+    void HookGripperPlugin::get_grasp_model_name(std::string& model_name, const std::string &c1, const std::string &c2) 
+    {
+        std::string model_name_ = "";
+        
+        std::size_t found1 = c1.find(this->objects_link_name);
+        if (found1 !=  std::string::npos) {
+            std::size_t end_pos = c1.find(":");
+            model_name_ = c1.substr(0, end_pos);
+        } else {
+            std::size_t found2 = c2.find(this->objects_link_name);
+            if (found2 !=  std::string::npos) {
+                std::size_t end_pos = c2.find(":");
+                model_name_ = c2.substr(0, end_pos);
+            } else {
+                gzmsg << "Error to find any model name in collision objects!\n";
+            }
+        }
+
+        model_name = model_name_;
     }
 
     void HookGripperPlugin::OnContactsMsg(ConstContactsPtr &_msg)
     {
         boost::mutex::scoped_lock lock(this->mutex);
 
+        bool ready_to_attach_ = false;
+
         msgs::Contacts contacts;
         contacts = (*_msg);
-        bool ready_to_attach = false;
+        
         for (int i = 0; i < contacts.contact_size(); ++i)
         {
             std::string c1 = contacts.contact(i).collision1();
             std::string c2 = contacts.contact(i).collision2();
+            if (!c1.empty() && !c2.empty()) {
 
-            if (c1.find(this->gripper_collision) !=  std::string::npos) {
-              if (c2.find(this->objects_link_name) !=  std::string::npos) {
-                for (int j = 0; j < contacts.contact(i).position_size(); ++j) {
-                    if (contacts.contact(i).depth(j) >= this->min_collision_depth) {
-                        ready_to_attach = true;
-
-                        // get model (robot) name
-                        std::size_t found = c2.find(":");
-                        if (found !=  std::string::npos) {
+                // check if gripper and ok-to-grasp object in collision
+                if (this->check_collision(c1, c2) || this->check_collision(c2, c1)) {
+                    for (int j = 0; j < contacts.contact(i).position_size(); ++j) {
+                        if (contacts.contact(i).depth(j) >= this->min_collision_depth) {
                             this->grasping_model_name.clear();
-                            this->grasping_model_name = c2.substr(0,found);
-                            gzmsg << "Collision between: " << contacts.contact(i).collision1() << " and " << contacts.contact(i).collision1() << std::endl;
-                            gzmsg << "grasping_model_name: " << this->grasping_model_name << std::endl;
-                            break;
-                        } else {
-                            gzmsg << "Error to find model name in collision string!\n";
-                        }
-                    }                
-                }
-              }
-            } else if (c2.find(this->gripper_collision) !=  std::string::npos) {
-              if (c1.find(this->objects_link_name) !=  std::string::npos) {
-                for (int j = 0; j < contacts.contact(i).position_size(); ++j) {
-                    if (contacts.contact(i).depth(j) >= this->min_collision_depth) {
-                        ready_to_attach = true;
-
-                        // get model (robot) name
-                        std::size_t found = c2.find(":");
-                        if (found !=  std::string::npos) {
-                            this->grasping_model_name.clear();
-                            this->grasping_model_name = c2.substr(0,found);
-                            gzmsg << "Collision between: " << contacts.contact(i).collision1() << " and " << contacts.contact(i).collision1() << std::endl;
-                            gzmsg << "grasping_model_name: " << this->grasping_model_name << std::endl;
-                            break;
-                        } else {
-                            gzmsg << "Error to find model name in collision string!\n";
-                        }
-
+                            std::string model_name;
+                            this->get_grasp_model_name(model_name, c1, c2);
+                            this->grasping_model_name = model_name;
+                            ready_to_attach_ = true;
+                        }                
                     }
                 }
-              }
-            } else {
-                continue;
+
             }
         } 
-        this->ready_to_attach = ready_to_attach;       
+        this->ready_to_attach = ready_to_attach_;       
     }
 
-    void HookGripperPlugin::Attach() {
-        boost::mutex::scoped_lock lock(this->mutex);
-
+    void HookGripperPlugin::Attach() 
+    {
+    
         physics::ModelPtr obj = this->model->GetWorld()->ModelByName(this->grasping_model_name);
         std::cerr << obj->GetLink()->WorldPose() << std::endl;
         std::cerr << this->palmLink->WorldPose() << std::endl;
 
         ignition::math::Pose3d diff = obj->GetLink()->WorldPose() - this->palmLink->WorldPose();
 
-        this->fixedJoint->Reset();
+        physics::PhysicsEnginePtr physics = this->model->GetWorld()->Physics();
+        this->fixedJoint = physics->CreateJoint("revolute"); // TODO move to constructor
+
+        // this->fixedJoint->Reset();
         this->fixedJoint->SetName("virtual_joint");
         this->fixedJoint->AddType(gazebo::physics::Base::FIXED_JOINT);
-        // this->fixedJoint->SetVelocityLimit(0,0);
-        // this->fixedJoint->SetVelocityLimit(1,0);
-        // this->fixedJoint->SetVelocityLimit(2,0);
         this->fixedJoint->Attach(this->palmLink, obj->GetLink());
         
         attached = !attached;
@@ -173,22 +201,29 @@ namespace gazebo
 
     void HookGripperPlugin::Detach() {
         this->fixedJoint->Detach();
+        this->fixedJoint->Fini();
         attached = !attached;
-    }
-
-    void HookGripperPlugin::OnReset()
-    {
     }
 
     void HookGripperPlugin::OnUpdate()
     {
+        // if ready to attach
         std_msgs::Bool grasping_msg;
         if (this->ready_to_attach) {
             grasping_msg.data = true;
         } else {
             grasping_msg.data = false;
         }
-        this->rosPubState.publish(grasping_msg);
+        this->rosPubReady.publish(grasping_msg);
+
+        // if attached
+        std_msgs::Bool attached_msg;
+        if (this->attached) {
+            attached_msg.data = true;
+        } else {
+            attached_msg.data = false;
+        }
+        this->rosPubState.publish(attached_msg);
     }
 
     void HookGripperPlugin::Connect() {
@@ -197,6 +232,37 @@ namespace gazebo
 
     void HookGripperPlugin::Disconnect() {
         this->state_publisher_connect_count_--;
+    }
+
+    void HookGripperPlugin::Connect2() {
+        this->ready_publisher_connect_count_++;
+    }
+
+    void HookGripperPlugin::Disconnect2() {
+        this->ready_publisher_connect_count_--;
+    }
+
+    bool HookGripperPlugin::GraspServiceCallback(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res)
+    {
+        boost::mutex::scoped_lock lock(this->mutex);
+        
+        if (req.data == true) {
+            if (this->ready_to_attach && !this->attached) {
+                this->Attach();
+                res.success = true;
+                res.message = this->grasping_model_name;
+            } else {
+                ROS_WARN("Can't grasp object. Move gripper closer to grasping object!");
+            }
+        } else {
+            if (this->attached) {
+                this->Detach();
+                res.success = false;
+                res.message = "move closer";
+            }
+        }
+        
+        return true;
     }
 
     void HookGripperPlugin::QueueThread()
